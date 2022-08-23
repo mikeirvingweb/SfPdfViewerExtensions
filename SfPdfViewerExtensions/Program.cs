242def9890fce13ff01e6badb5d93c2bc32c2bba
Mike Irving <c-mirving@kahua.com>
2022-08-23 11:02:27 +0100

    Add project files.

---
+++ SfPdfViewerExtensions/Program.cs
@@ -0,0 +1,27 @@
+﻿using SfPdfViewerExtensions;
+using Syncfusion.Drawing;
+using Syncfusion.SfPdfViewer.XForms;
+
+// Examples
+
+SfPdfViewer sfPdfViewer = new SfPdfViewer();
+
+/* 
+    your code here,
+    load your PDF etc
+*/
+
+// get Size of Page 1. returns Size
+Size pageSize = sfPdfViewer.PageSize(1);
+
+// get Width of Page 1. returns int
+int pageWidth = sfPdfViewer.PageWidth(1);
+
+// get Height of Page 1. returns int
+int pageHeight = sfPdfViewer.PageHeight(1);
+
+// is Page 1 Landscape? returns bool
+bool pageIsLandscape = sfPdfViewer.PageIsLandscape(1);
+
+// is Page 1 Portrait? returns bool
+bool pageIsPortrait = sfPdfViewer.PageIsPortrait(1);
---
+++ SfPdfViewerExtensions/SfPdfViewerExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Syncfusion.Drawing;
+using Syncfusion.SfPdfViewer.XForms;
+
+namespace SfPdfViewerExtensions
+{
+    public static class SfPdfViewerExtensions
+    {
+        public static Size PageSize(this SfPdfViewer sfPdfViewer, int page)
+        {
+            return new Syncfusion.Drawing.Image(sfPdfViewer.ExportAsImage(page - 1)).Size;
+        }
+
+        public static int PageWidth(this SfPdfViewer sfPdfViewer, int page)
+        {
+            return PageSize(sfPdfViewer, page).Width;
+        }
+
+        public static int PageHeight(this SfPdfViewer sfPdfViewer, int page)
+        {
+            return PageSize(sfPdfViewer, page).Height;
+        }
+
+        public static bool PageIsLandscape(this SfPdfViewer sfPdfViewer, int page)
+        {
+            var pageSize = PageSize(sfPdfViewer, page);
+
+            return pageSize.Width > pageSize.Height;
+        }
+
+        public static bool PageIsPortrait(this SfPdfViewer sfPdfViewer, int page)
+        {
+            var pageSize = PageSize(sfPdfViewer, page);
+
+            return pageSize.Height > pageSize.Width;
+        }
+    }
+}
